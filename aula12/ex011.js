80121603d64db80e9a0a1d7c7e8aedaf02350ade
Guilherme Teixeira Santos <41587365+guiteixeira-io@users.noreply.github.com>
2024-04-11 01:21:44 -0300

    Aula 12

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 18
+console.log(`Voce te m ${idade} anos`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade <18 || idade > 65) {
+        console.log('Voto opicional')
+}
+        else {
+            console.log('Voto obrigatorio')
+
+}
